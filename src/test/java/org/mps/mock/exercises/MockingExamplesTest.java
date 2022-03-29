4a304153b2634df2c9121293cc1d9f781bce9e3f
nicoZhao1908 <0619971117@uma.es>
2022-03-29 16:51:30 +0200

    Update MockingExamplesTest class with Buffer, Box, Item classes

--- src/test/java/org/mps/mock/exercises/MockingExamplesTest.java
+++ src/test/java/org/mps/mock/exercises/MockingExamplesTest.java
@@ -5,15 +5,129 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
 public class MockingExamplesTest {
 
-  private class ExampleClass1 {
-  }
+    private class Buffer {
+        List<Box> boxes;
+        int totalSize;
 
-  @Test
-  public void test() {
-    ExampleClass1 example = Mockito.mock(ExampleClass1.class) ;
+        Buffer(){
+            boxes = new ArrayList<>();
+            totalSize = 0;
+        }
+        void insertBox(Box box){
+            boxes.add(box);
+            totalSize++;
+        }
+        void removeBox(Box box){
+            boxes.remove(box);
+            totalSize--;
+        }
+        int getTotalSize(){
+            return totalSize;
+        }
+        Enumeration<Box> getBoxes(){
+            return java.util.Collections.enumeration(boxes);
+        }
 
-    assertNotNull(example) ;
-  }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Buffer buffer = (Buffer) o;
+
+            if (totalSize != buffer.totalSize) return false;
+            return boxes != null ? boxes.equals(buffer.boxes) : buffer.boxes == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = boxes != null ? boxes.hashCode() : 0;
+            result = 31 * result + totalSize;
+            return result;
+        }
+    }
+
+    private class Item {
+        int content;
+
+        Item(int content) {
+            content = this.content;
+        }
+
+        int getContent() {
+            return content;
+        }
+
+        void setContent(int content) {
+            this.content = content;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Item item = (Item) o;
+
+            return content == item.content;
+        }
+
+        @Override
+        public int hashCode() {
+            return content;
+        }
+    }
+
+    private class Box {
+        List<Item> items;
+        int totalSize;
+        Box(){
+            items = new ArrayList<>();
+        }
+        void insertItem(Item item){
+            items.add(item);
+            totalSize++;
+        }
+        void removeItem(Item item){
+            items.remove(item);
+            totalSize--;
+        }
+        int getTotalSize(){
+            return totalSize;
+        }
+        Enumeration<Item> getItems(){
+            return java.util.Collections.enumeration(items);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Box box = (Box) o;
+
+            if (totalSize != box.totalSize) return false;
+            return items != null ? items.equals(box.items) : box.items == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = items != null ? items.hashCode() : 0;
+            result = 31 * result + totalSize;
+            return result;
+        }
+    }
+
+    @Test
+    public void test() {
+        Buffer example = Mockito.mock(Buffer.class);
+
+        assertNotNull(example);
+    }
 }
